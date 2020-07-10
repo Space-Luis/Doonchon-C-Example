2bc12bdf01556e9a231d725835a73a9a331eb66a
Space-Luis <57285437+Space-Luis@users.noreply.github.com>
2020-07-11 00:23:15 +0900

    Create Base_Operator.c

---
+++ 4차시 - 연산자예제/Base_Operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main(){ 
+
+    int num1 = 2;
+    int num2 = 1;
+
+    printf("%d", num1 + num2); // num1의 값과 num2의 값을 더해서 출력
+    printf("%d", num1 - num2); // num1의 값과 num2의 값을 빼서 출력
+    printf("%d", num1 * num2); // num1의 값과 num2의 값을 곱해서 출력
+    printf("%d", num1 / num2); // num1의 값과 num2의 값을 나눠서 출력
+    printf("%d", num1 % num2); // num1의 값과 num2의 값의 나머지를 출력
+    
+    return 0;
+}
