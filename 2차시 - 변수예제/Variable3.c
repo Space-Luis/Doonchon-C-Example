2c1f901472c5f85688530cd0a21e67bf8a363190
Space-Luis <57285437+Space-Luis@users.noreply.github.com>
2020-07-10 23:58:43 +0900

    Create Variable3.c

---
+++ 2차시 - 변수예제/Variable3.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main(){ 
+    
+    int num1 = 10;          // 변수를 선언하면서 값 할당 ( 초기화 )
+    int num2 = 20;
+    int num3 = 30;
+
+    printf("%d\n", num1);   // 10
+    printf("%d\n", num2);   // 20
+    printf("%d\n", num3);   // 30
+
+    return 0;
+}
