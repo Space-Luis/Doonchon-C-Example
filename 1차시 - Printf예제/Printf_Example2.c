11eff5347898f6652343f2420305c141258bc497
Space-Luis <57285437+Space-Luis@users.noreply.github.com>
2020-07-10 23:47:34 +0900

    Create Printf_Example2.c

---
+++ 1차시 - Printf예제/Printf_Example2.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main(){ 
+    // int : 정수                   서식지정자 : %d
+    // float : 실수                 서식지정자 : %f
+    // char : 문자                  서식지정자 : %c
+    // char[] : 문자열              서식지정자 : %s
+    
+    printf("%d\n", 1004); 
+    printf("%f\n", 3.1415);
+    printf("%c\n", "A"); 
+    printf("%s\n", "Hello, World!");
+    
+    return 0;
+}
